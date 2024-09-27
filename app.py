eb90adcfad820394782fd546e7afd65cd313090c
Xiao2020 <35255094+hsiaohofu@users.noreply.github.com>
2024-09-27 11:47:44 +0800

    f

--- app.py
+++ app.py
@@ -9,22 +9,22 @@ def home():
 @app.route('/med')
 def med():
     return render_template('med.html')
-
-@app.route('/surg')
-def surg():
-    return render_template('surg.html')
-
-@app.route('/ped')
-def ped():
-    return render_template('ped.html')
-
-@app.route('/obs')
-def obs():
-    return render_template('obs.html')
-
-@app.route('/other')
-def other():
-    return render_template('other.html')
+#
+# @app.route('/surg')
+# def surg():
+#     return render_template('surg.html')
+#
+# @app.route('/ped')
+# def ped():
+#     return render_template('ped.html')
+#
+# @app.route('/obs')
+# def obs():
+#     return render_template('obs.html')
+#
+# @app.route('/other')
+# def other():
+#     return render_template('other.html')
 
 if __name__ == '__main__':
     app.run()
